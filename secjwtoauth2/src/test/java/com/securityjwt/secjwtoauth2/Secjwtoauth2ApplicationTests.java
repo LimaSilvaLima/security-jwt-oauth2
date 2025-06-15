4be44d240885faae06c577a2f08b8fb65793f1a1
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-15 18:44:05 -0300

    projeto iniciado rodando

---
+++ secjwtoauth2/src/test/java/com/securityjwt/secjwtoauth2/Secjwtoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityjwt.secjwtoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secjwtoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
