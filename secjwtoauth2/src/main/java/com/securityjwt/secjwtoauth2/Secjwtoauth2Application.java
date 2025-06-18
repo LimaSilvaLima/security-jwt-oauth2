0612ed21bf04b587be2c79f3a9ea3809fb3607cd
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-17 21:24:47 -0300

    concluida a primeira parte com autenticação padrao do spring

--- secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/Secjwtoauth2Application.java
+++ secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/Secjwtoauth2Application.java
@@ -11,3 +11,4 @@ public class Secjwtoauth2Application {
 	}
 
 }
+ 
--- secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/infra/security/SecurityConfig.java
+++ secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/infra/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.securityjwt.secjwtoauth2.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,8 +16,11 @@ public class SecurityConfig {
         return http
             .authorizeHttpRequests(
                 authorizeConfig ->{
+                    authorizeConfig.requestMatchers("/public").permitAll();
+                    authorizeConfig.requestMatchers("logout").permitAll();
                     authorizeConfig.anyRequest().authenticated();
-                })      
+                })
+            .formLogin(Customizer.withDefaults())      
             .build();
                 
     }
