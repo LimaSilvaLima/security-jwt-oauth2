b312433b2d627cf175c689448ac13da51976202b
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-20 14:41:31 -0300

    autenticando conta google retornando identificação de user e token

--- secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/controllers/HttpController.java
+++ secjwtoauth2/src/main/java/com/securityjwt/secjwtoauth2/controllers/HttpController.java
@@ -1,6 +1,8 @@
 package com.securityjwt.secjwtoauth2.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -11,11 +13,22 @@ public class HttpController {
     @GetMapping("/public")
      public String publicRoute() {
         return "<h1>Public Route </h1><p>This route is accessible to everyone.</p>";
+        
     }
 
     @GetMapping("/private")
-    public String privateRoute() {
-        return "<h1>Private Route </h1><p>This route is accessible only to authenticated users.</p>";
+    public String privateRoute(@AuthenticationPrincipal OidcUser principal) {
+        //return "<h1>Private Route </h1><p>This route is accessible only to authenticated users.</p>";
+        return String.format("""
+            <h1>Public Route <p>This route is accessible to everyone</h1>
+            <h3>Principal : %s</h3>
+            <h3>Email attribute : %s</h3>
+            <h3>Authorities : %s</h3>
+            <h3>JWT: %s</h3>
+            """, principal, principal.getAttribute("email"),
+            principal.getAuthorities(),
+            principal.getIdToken().getTokenValue());
+            
     }   
     
 
